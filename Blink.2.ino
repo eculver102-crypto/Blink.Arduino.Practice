5a553ec55d256979b6faaccd1f4cc2f114f823c1
Emily Culver <eculver102@gmail.com>
2026-01-22 12:47:00 -0500

    Revert "Update Blink.2.ino"
    This reverts commit a4b5967c84b7ee56ecf7dc89c7c42dca34ff3083.

--- Blink.2.ino
+++ Blink.2.ino
@@ -27,10 +27,7 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
-const int PWMB = 10; //speed control pin on the motor driver for the left motor
-const int BIN2 = 9;  //control pin 2 on the motor driver for the left motor
-const int BIN1 = 8;  //control pin 1 on the motor driver for the left motor
- 
+
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
